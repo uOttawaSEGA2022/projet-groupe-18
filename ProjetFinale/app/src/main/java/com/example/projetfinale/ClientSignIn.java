56bb1c8a7b221a68b0dbbe6803d32b8519ec65d0
MemesWillOutliveUsAll <114416994+LSavkovic@users.noreply.github.com>
2022-10-20 22:10:44 -0400

    Layout update 2

--- ProjetFinale/app/src/main/java/com/example/projetfinale/ClientSignIn.java
+++ ProjetFinale/app/src/main/java/com/example/projetfinale/ClientSignIn.java
@@ -2,7 +2,9 @@ package com.example.projetfinale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClientSignIn extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ClientSignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_sign_in);
+    }
+
+    public void OnClientSignIn(View view){
+        Intent intent = new Intent(getApplicationContext(),Bienvenue.class);
+        startActivityForResult(intent,0);
     }
 }
--- ProjetFinale/app/src/main/java/com/example/projetfinale/CuisinierSignIn.java
+++ ProjetFinale/app/src/main/java/com/example/projetfinale/CuisinierSignIn.java
@@ -2,7 +2,9 @@ package com.example.projetfinale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CuisinierSignIn extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CuisinierSignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuisinier_sign_in);
+    }
+
+    public void OnCuisinierSignIn(View view){
+        Intent intent = new Intent(getApplicationContext(),Bienvenue.class);
+        startActivityForResult(intent,0);
     }
 }
