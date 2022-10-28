2766137d26fcafb43995770e7eb84fcf829d4484
S1monMarch <114030559+S1monMarch@users.noreply.github.com>
2022-10-28 17:40:36 -0400

    Add files via upload

---
+++ CuisinerLogin.java
@@ -0,0 +1,66 @@
+package com.example.projetfinale;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CuisinerLogin extends AppCompatActivity {
+    EditText Email;
+    EditText Password;
+    Button Login;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuisiner_login);
+
+        Email = findViewById(R.id.editTextTextPersonName);
+        Password = findViewById(R.id.editTextTextPersonName2);
+        Login = findViewById(R.id.CuisinierLoginButton);
+        mAuth = FirebaseAuth.getInstance();
+        Login.setOnClickListener(view -> {
+            LoginUser();
+        });
+    }
+    private void LoginUser(){
+        String email = Email.getText().toString();
+        String password = Password.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            Email.setError("Email ne peut pas être vide");
+            Email.requestFocus();
+        }else if(TextUtils.isEmpty(password)) {
+            Password.setError("Password ne peut pas être vide");
+            Password.requestFocus();
+        }else{
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(CuisinerLogin.this, "Utilisatuer a entrer sans problèmes", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CuisinerLogin.this, Bienvenue.class));
+                    } else {
+                        Toast.makeText(CuisinerLogin.this, "Erreur de login" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
+    }
+    public void OnCuisinierLogin(View view){
+        Intent intent = new Intent(getApplicationContext(),Bienvenue.class);
+        startActivityForResult(intent,0);
+    }
+}
---
+++ CuisinierSignIn.java
@@ -0,0 +1,85 @@
+package com.example.projetfinale;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CuisinierSignIn extends AppCompatActivity {
+    EditText FirstName;
+    EditText LastName;
+    EditText Email;
+    EditText Address;
+    EditText Password;
+    FirebaseAuth mAuth;
+    Button CuisinierSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuisinier_sign_in);
+
+        FirstName = findViewById(R.id.editTextTextPersonName4);
+        LastName = findViewById(R.id.editTextTextPersonName7);
+        Email = findViewById(R.id.editTextTextPersonName8);
+        Address = findViewById(R.id.editTextTextPersonName3);
+        Password = findViewById(R.id.editTextTextPersonName5);
+        CuisinierSignIn = findViewById(R.id.clientSignIn);
+
+        mAuth = FirebaseAuth.getInstance();
+        CuisinierSignIn.setOnClickListener(view ->{
+            createUser();
+        });
+    }
+    private void createUser(){
+        String firstName = FirstName.getText().toString();
+        String lastName = LastName.getText().toString();
+        String email = Email.getText().toString();
+        String address = Address.getText().toString();
+        String password = Password.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            Email.setError("Email ne peut pas être vide");
+            Email.requestFocus();
+        }else if(TextUtils.isEmpty(password)){
+            Password.setError("Password ne peut pas être vide");
+            Password.requestFocus();
+        }else if(TextUtils.isEmpty(firstName)){
+            FirstName.setError("First name ne peut pas être vide");
+            FirstName.requestFocus();
+        }else if(TextUtils.isEmpty(lastName)){
+            LastName.setError("Last name ne peut pas être vide");
+            LastName.requestFocus();
+        }else if(TextUtils.isEmpty(address)){
+            Address.setError("Address ne peut pas être vide");
+            Address.requestFocus();
+        }else{
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(CuisinierSignIn.this, "Utilisatuer a entrer sans problèmes", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CuisinierSignIn.this, CuisinerLogin.class));
+                    }else{
+                        Toast.makeText(CuisinierSignIn.this, "Erreur de régistration" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
+    }
+    public void OnCuisinierSignIn(View view){
+        Intent intent = new Intent(getApplicationContext(),Bienvenue.class);
+        startActivityForResult(intent,0);
+    }
+}
