1d48ee2336017f5e736ac6fa7906ac6ea24607cf
MemesWillOutliveUsAll <114416994+LSavkovic@users.noreply.github.com>
2022-10-28 17:48:07 -0400

    Revert "Merge pull request #1 from uOttawaSEGA2022/S1monMarch-patch-1"
    This reverts commit bb4d40d3f6661c079d7a49e33af6b00ccf38a2de, reversing
    changes made to 15ea713cc0fa13abe6e0aca8cd8c9498b27c2140.

--- Bienvenue.java
+++ Bienvenue.java
@@ -5,34 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Bienvenue extends AppCompatActivity {
-    Button returnToMain;
-    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenue);
-
-        returnToMain = findViewById(R.id.returnToMain);
-        mAuth = FirebaseAuth.getInstance();
-        returnToMain.setOnClickListener(view ->{
-            mAuth.signOut();
-            startActivity(new Intent(Bienvenue.this, MainActivity.class));
-        });
-    }
-    @Override
-    protected void onStart(){
-        super.onStart();
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user == null){
-            startActivity(new Intent(Bienvenue.this, ClientLogin.class ));
-        }
     }
 
     public void OnReturn(View view){
--- MainActivity.java
+++ MainActivity.java
@@ -3,63 +3,20 @@ package com.example.projetfinale;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    Button ClientLogin;
-    Button ClientSignIn;
-    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ClientLogin = findViewById(R.id.LCC);
-        ClientSignIn = findViewById(R.id.Client);
-        mAuth = FirebaseAuth.getInstance();
-        ClientLogin.setOnClickListener(view -> {
-            LoginClient();
-        });
-        ClientSignIn.setOnClickListener(view -> {
-            SignInClient();
-        });
     }
 
     public void OnBienvenue(View view){
         Intent intent = new Intent(getApplicationContext(),Bienvenue.class);
         startActivityForResult(intent,0);
-    }
-
-    public void OnClientLogin(View view){
-        Intent intent = new Intent(getApplicationContext(),ClientLogin.class);
-        startActivityForResult(intent,0);
-    }
-
-    public void OnClientSignIn(View view){
-        Intent intent = new Intent(getApplicationContext(),ClientSignIn.class);
-        startActivityForResult(intent,0);
-    }
-
-    public void OnCookerSignIn(View view){
-        Intent intent = new Intent(getApplicationContext(),CuisinierSignIn.class);
-        startActivityForResult(intent,0);
-    }
-
-    public void OnCookerLogin(View view){
-        Intent intent = new Intent(getApplicationContext(),CuisinerLogin.class);
-        startActivityForResult(intent,0);
-    }
-
-    private void LoginClient() {
-        startActivity(new Intent(MainActivity.this, ClientLogin.class));
-    }
-    private void SignInClient(){
-        startActivity(new Intent(MainActivity.this, ClientSignIn.class));
     }
 }
