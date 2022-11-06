a29b44297ef71d975e1f45a33e20b1737c685cf6
angue047 <113554333+angue047@users.noreply.github.com>
2022-11-06 17:36:13 -0500

    fixed interface, added admin functions, organized layouts

--- ProjetFinale/app/src/main/java/com/example/projetfinale/Client.java
+++ ProjetFinale/app/src/main/java/com/example/projetfinale/Client.java
@@ -2,6 +2,7 @@ package com.example.projetfinale;
 
 public class Client extends User{
     private String address;
+
     public Client(String firstName, String lastName, String emailAddress, String password, String address){
         super(firstName,lastName,emailAddress,password);
         this.address = address;
--- ProjetFinale/app/src/main/java/com/example/projetfinale/CuisinerLogin.java
+++ ProjetFinale/app/src/main/java/com/example/projetfinale/CuisinerLogin.java
@@ -11,7 +11,7 @@ public class CuisinerLogin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cuisiner_login);
+        setContentView(R.layout.activity_cook_login);
     }
 
     public void OnCuisinierLogin(View view){
