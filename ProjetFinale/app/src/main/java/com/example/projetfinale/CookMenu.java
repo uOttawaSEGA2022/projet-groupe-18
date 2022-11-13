f151bdf3b2c5247a93ec7570187be533b812cc87
angue047 <113554333+angue047@users.noreply.github.com>
2022-11-13 16:04:06 -0500

    cook and cook menu changed

--- ProjetFinale/app/src/main/java/com/example/projetfinale/CookMenu.java
+++ ProjetFinale/app/src/main/java/com/example/projetfinale/CookMenu.java
@@ -1,21 +1,19 @@
 package com.example.projetfinale;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseUser;
 
-public class CookerMenu extends AppCompatActivity {
+public class CookMenu extends AppCompatActivity {
     Button returnToMain;
     FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cooker_menu);
 
@@ -29,13 +27,14 @@ public class CookerMenu extends AppCompatActivity {
     @Override
     protected void onStart(){
         super.onStart();
+
         FirebaseUser user = mAuth.getCurrentUser();
         if (user == null){
             startActivity(new Intent(CookerMenu.this, ClientLogin.class ));
         }
     }
 
-    public void OnReturn(View view){
+    public void OnReturn( View view){
         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
         startActivityForResult(intent,0);
     }
