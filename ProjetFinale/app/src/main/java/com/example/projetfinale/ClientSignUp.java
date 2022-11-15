42de78e2e00ca00c6631457d2f07540139497c15
a-taillardat <atail082@uottawa.ca>
2022-11-15 09:21:49 -0500

    Client Sign Up update
    Work on ClientSignUp.java and its layout; need more work and extract Firebase operations out of it to put them in the Client class

--- ProjetFinale/app/src/main/java/com/example/projetfinale/ClientSignUp.java
+++ ProjetFinale/app/src/main/java/com/example/projetfinale/ClientSignUp.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -16,25 +17,28 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Pattern;
+
 public class ClientSignUp extends AppCompatActivity {
-    EditText FirstName;
-    EditText LastName;
-    EditText Email;
-    EditText Address;
-    EditText Password;
+
+    private EditText FirstName;
+    private EditText LastName;
+    private EditText Email;
+    private EditText Address;
+    private EditText Password;
     FirebaseAuth mAuth;
-    Button clientSignIn;
+    private Button clientSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_sign_up);
 
-        FirstName = findViewById(R.id.editTextTextPersonName4);
-        LastName = findViewById(R.id.editTextTextPersonName7);
-        Email = findViewById(R.id.editTextTextPersonName8);
-        Address = findViewById(R.id.editTextTextPersonName3);
-        Password = findViewById(R.id.editTextTextPersonName5);
+        FirstName = findViewById(R.id.firstNameField);
+        LastName = findViewById(R.id.lastNameField);
+        Email = findViewById(R.id.emailField);
+        Password = findViewById(R.id.passwordField);
+        Address = findViewById(R.id.addressField);
         clientSignIn = findViewById(R.id.clientSignIn);
 
         mAuth = FirebaseAuth.getInstance();
@@ -43,6 +47,7 @@ public class ClientSignUp extends AppCompatActivity {
         });
     }
     private void createUser(){
+
         String firstName = FirstName.getText().toString();
         String lastName = LastName.getText().toString();
         String email = Email.getText().toString();
@@ -80,6 +85,83 @@ public class ClientSignUp extends AppCompatActivity {
     }
 
     public void OnClientSignIn(View view) {
-        startActivity(new Intent(ClientSignUp.this, ClientMenu.class));
+
+        /*
+
+        String firstName = String.valueOf(FirstName.getText());
+        String lastName = String.valueOf(LastName.getText());
+        String email = String.valueOf(Email.getText());
+        String password = String.valueOf(Password.getText());
+        String address = String.valueOf(Address.getText());
+
+        // Checking entries
+        boolean wrongEntry = false;
+        if (firstName == null) {
+            FirstName.setTextColor(Color.RED);
+            wrongEntry = true;
+        }
+        if (lastName == null) {
+            LastName.setTextColor(Color.RED);
+            wrongEntry = true;
+        }
+        if (email == null) {
+            Email.setTextColor(Color.RED);
+            wrongEntry = true;
+        }
+        if (password == null) {
+            Password.setTextColor(Color.RED);
+            wrongEntry = true;
+        }
+        if (address == null) {
+            Address.setTextColor(Color.RED);
+            wrongEntry = true;
+        }
+        // Extra check for emails
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(emailRegex);
+        if (pat.matcher(email).matches()) {
+            Email.setTextColor(Color.RED);
+            wrongEntry = true;
+        }
+
+        if (! wrongEntry) {
+            try {
+                Client client = new Client(firstName, lastName, email, password, address);
+                startActivity(new Intent(ClientSignUp.this, ClientMenu.class));
+                return;
+            } catch (IllegalArgumentException e) {
+                wrongEntry = true;
+            }
+        }
+
+        Toast.makeText(ClientSignUp.this, "Please provide valid entries",
+                Toast.LENGTH_LONG).show();
+
+
+         */
+
+        createUser();
+
     }
+
+
+    /*  Put following firebase update in the Client class
+
+    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(ClientSignUp.this, "Utilisatuer a entrer sans problèmes", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ClientSignUp.this, ClientLogin.class));
+                    }else{
+                        Toast.makeText(ClientSignUp.this, "Erreur de régistration" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+     */
 }
