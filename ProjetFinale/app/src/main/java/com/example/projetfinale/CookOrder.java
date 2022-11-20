b52e600213b2ef4c4064e8737db2a60c7cb4a33d
angue047 <113554333+angue047@users.noreply.github.com>
2022-11-20 06:55:09 -0500

    cook update

---
+++ ProjetFinale/app/src/main/java/com/example/projetfinale/CookOrder.java
@@ -0,0 +1,14 @@
+package com.example.projetfinale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CookOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_manage_orders);
+    }
+}
---
+++ ProjetFinale/app/src/main/java/com/example/projetfinale/activity_cook_manage_meals.java
@@ -0,0 +1,14 @@
+package com.example.projetfinale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_cook_manage_meals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_manage_meals);
+    }
+}
